1e0fc8248023b326f45579497dd7a246c061af37
Ryan Smillie <RyanJSmillie@hotmail.co.uk>
2022-05-24 21:31:03 +0100

    controller and routes

---
+++ utils/drop-database.js
@@ -0,0 +1,25 @@
+// utils/create-database.js
+// require the promise version of mysql2
+const mysql = require('mysql2/promise');
+
+// require path to handle file paths
+const path = require('path');
+
+// load environment variables from env files
+require('dotenv').config({
+  path: path.join(__dirname, '../.env.test'),
+});
+
+// destructure environment variables from process.env 
+const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
+
+// connects with the sql database
+const connection = mysql.createConnection({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+  });
+  
+  // runs the query to drop the database
+  connection.query(`DROP DATABASE ${DB_NAME}`, () => connection.end());
